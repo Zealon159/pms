afd242e6d67545a16c5070c7e912666b8142880f
Zealon159 <zealon@126.com>
2017-07-11 23:17:11 +0800

    111

--- pms-web/src/main/java/cn/corpdata/pms/controller/org/UserController.java
+++ pms-web/src/main/java/cn/corpdata/pms/controller/org/UserController.java
@@ -1,11 +1,23 @@
 package cn.corpdata.pms.controller.org;
 
-import cn.corpdata.pms.service.org.impl.UserServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import cn.corpdata.pms.service.org.impl.UserServiceImpl;
+import cn.jpush.api.common.connection.IHttpClient.RequestMethod;
+
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 	private UserServiceImpl userService = new UserServiceImpl();
-	public String addUser(){
-		userService.addUser(null);
-		return "/addUser";
+	
+	@RequestMapping(method=RequestMethod.GET,name="/getJsonData/{keyword}",consumes="")
+	public @ResponseBody String addUser(@PathVariable String keyword){
+		//userService.addUser();
+		return keyword;
 	}
 }
